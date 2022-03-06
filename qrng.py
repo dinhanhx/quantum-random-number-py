b5167fd8ec9b132c3127adffe20af8e35273f6f6
Dinh Anh <dinhanhx@gmail.com>
2022-03-06 10:06:59 +0700

    Please PyLint

--- qrng.py
+++ qrng.py
@@ -1,22 +1,21 @@
 import requests
 
-def construct_link(array_length: int = 1, 
-                   data_type: str = 'uint8', 
+def construct_link(array_length: int = 1,
+                   data_type: str = 'uint8',
                    block_size: int = 1):
-    """Construct a link from 
+    """Construct a link from
 
     https://qrng.anu.edu.au/contact/api-documentation/
 
     Args:
         array_length (int): The amount of numbers must be between 1-1024
         data_type (str): uint8, uint16, hex16
-        block_size (int): only needed for hex16 data type. 
+        block_size (int): only needed for hex16 data type.
             Sets the length of each block. Must be between 1-1024.
     """
     # Make sure array length in 1-1024
     if array_length < 1: array_length = 1
     if array_length > 1024: array_length = 1024
-    
     # Make sure data type in available set
     if data_type in ('uint8', 'uint16', 'hex16'):
         if data_type == 'hex16':
@@ -25,7 +24,6 @@ def construct_link(array_length: int = 1,
             if block_size > 1024: block_size = 1024
             return (f'https://qrng.anu.edu.au/API/jsonI.php?'
                     f'length={array_length}&type={data_type}&size={block_size}')
-        
         else:
             return (f'https://qrng.anu.edu.au/API/jsonI.php?'
                     f'length={array_length}&type={data_type}')
@@ -34,10 +32,9 @@ def construct_link(array_length: int = 1,
         data_type = 'uint8'
         return (f'https://qrng.anu.edu.au/API/jsonI.php?'
                 f'length={array_length}&type={data_type}')
-                
 
 def get_data(link: str):
-    """Get data list from json response by requesting to link 
+    """Get data list from json response by requesting to link
 
     https://qrng.anu.edu.au/contact/api-documentation/
 
@@ -56,8 +53,8 @@ def get_random_uint8(array_length: int=1):
     https://qrng.anu.edu.au/contact/api-documentation/
 
     Args:
-        array_length (int, optional): The amount of numbers 
-            must be between 1-1024. 
+        array_length (int, optional): The amount of numbers
+            must be between 1-1024.
             Defaults to 1.
 
     Returns:
@@ -73,8 +70,8 @@ def get_random_uint16(array_length: int=1):
     https://qrng.anu.edu.au/contact/api-documentation/
 
     Args:
-        array_length (int, optional): The amount of numbers 
-            must be between 1-1024. 
+        array_length (int, optional): The amount of numbers
+            must be between 1-1024.
             Defaults to 1.
 
     Returns:
@@ -91,8 +88,8 @@ def get_random_hex16(array_length: int=1, block_size: int=1):
     https://qrng.anu.edu.au/contact/api-documentation/
 
     Args:
-        array_length (int, optional): The amount of numbers 
-            must be between 1-1024. 
+        array_length (int, optional): The amount of numbers
+            must be between 1-1024.
             Defaults to 1.
 
     Returns:
@@ -106,3 +103,4 @@ if '__main__' == __name__:
     print(get_random_uint8(10))
     print(get_random_uint16(10))
     print(get_random_hex16(10, block_size=2))
+    
